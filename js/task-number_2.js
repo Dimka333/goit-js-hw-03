9657a22ff15db51456408ab9555e847ce7b5017f
Dmytro Kuzminskyi <Dimka333@users.noreply.github.com>
2020-12-15 23:50:41 +0200

    ДЗ 3

---
+++ js/task-number_2.js
@@ -0,0 +1,14 @@
+const countProps = function(obj) {
+    const keys = Object[keys].length(countProps);
+    console.log(keys);
+};
+
+
+
+
+console.log(countProps({}));
+
+console.log(countProps({ name: 'Mango', age: 2 }));
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+console.log(countProps);
